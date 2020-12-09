18a6ed64ae829b402f7a2619cd6501d10e69fe7a
verylostpenguin <willhong9@gmail.com>
2020-12-09 18:35:48 -0500

    Changes to physics formulas

--- Body.py
+++ Body.py
@@ -6,18 +6,26 @@ from Quadtree import *
 
 
 class Body:
-  def __init__(self, canvas, pos, vel, mass, radius, num, color, charge, space = None):
+  def __init__(self, canvas, pos, vel, mass, radius, num, color, charge, name, space = None):
     self.num = num
     self.canvas = canvas
     self.color = color
-    self.id = canvas.create_oval(pos[0] - radius, pos[1] - radius, pos[0] + radius, pos[1] + radius, fill=color)
-    self.lineID = self.canvas.create_line(pos[0], pos[1], pos[0] + 10 * vel[0], pos[1] + 10 * vel[1])
+    self.name = name
     self.charge = charge # Can add later
     self.selected = False
     
-    self.position = pos
-    self.velocity = vel
+    self.scale = 1
+    self.realPos = pos
+    self.realVel = vel
+    self.mom = mass * vel
+    self.velocity = vel * self.scale
+    self.position = pos * self.scale + np.array([600, 400], dtype="float64")
     self.force = np.array([0.0, 0.0])
+
+    self.id = canvas.create_oval(self.position[0] - radius, self.position[1] - radius, self.position[0] + radius, 
+                                 self.position[1] + radius, fill=color)
+    self.lineID = self.canvas.create_line(self.position[0], self.position[1], self.position[0] + 10 * self.velocity[0],
+                                          self.position[1] + 10 * self.velocity[1])
     
     self.radius = radius
     self.mass = mass
@@ -29,36 +37,40 @@ class Body:
   @staticmethod
   def pair_Gforce(body_pair, space):
     body1, body2 = body_pair
-    dpos = (body2.position - body1.position)
+    dpos = (body2.realPos - body1.realPos)
     force = (dpos/np.linalg.norm(dpos)**3)*space.G*body1.mass*body2.mass
     body1.force += force
     body2.force -= force
 
-  @staticmethod
-  def pair_Eforce(body_pair, space):
-    body1, body2 = body_pair
-    dpos = (body2.position - body1.position)
-    force = (dpos/np.linalg.norm(dpos)**3)*space.k*body1.mass*body2.mass
-    sign = body1.charge * body2.charge
-    body1.force -= force * sign
-    body2.force += force * sign
+  # @staticmethod
+  # def pair_Eforce(body_pair, space):
+  #   body1, body2 = body_pair
+  #   dpos = (body2.realPos - body1.realPos)
+  #   force = (dpos/np.linalg.norm(dpos)**3)*space.k*body1.mass*body2.mass
+  #   sign = body1.charge * body2.charge
+  #   body1.force -= force * sign
+  #   body2.force += force * sign
 
   def contains(self, pos):
     return np.linalg.norm(self.position - pos) < self.radius/2
 
   def move(self, dt):
-    momentum = self.force * dt * 1e-10**2 # scale back down to render
-    self.velocity += momentum / self.mass
-    self.position += self.velocity * dt
-    print(self.velocity, self.position)
+    self.mom += self.force * dt
     
-    self.canvas.move(self.id, self.velocity[0], self.velocity[1])
-    self.force = np.array([0.0, 0.0])
-  
+    self.realVel = self.mom / self.mass
+    self.velocity = self.realVel * self.scale
+
+    self.realPos += self.realVel * dt
+    self.position = self.realPos * self.scale
+
+    self.canvas.coords(self.id, int(self.position[0] - self.radius) + 600, int(self.position[1] - self.radius) + 400,
+                       int(self.position[0] + self.radius) + 600, int(self.position[1] + self.radius) + 400)
+    self.force = np.zeros(2)
+      
   def updateVector(self):
     self.canvas.delete(self.lineID)
     self.lineID = self.canvas.create_line(self.position[0],self.position[1],self.position[0]+10*self.velocity[0],
-                                          self.position[1]+10*self.velocity[1], fill = "white")
+                                          self.position[1]+10*self.mom[1], fill = "white")
 
   def updateShape(self, flipCharge=False, mass=None):
     if flipCharge:
